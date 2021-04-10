2d0e4dfd876a52db28faf414f8bccb5b746cd473
laoshufeifei <laoshufeifei@qq.com>
2021-04-10 15:04:31 +0800

    tweak

--- src/other/bits/bits.go
+++ src/other/bits/bits.go
@@ -1,5 +1,14 @@
 package bits
 
+func isPowerOfTwo(n uint64) bool {
+	if n == 0 {
+		return false
+	}
+
+	return (n & (n - 1)) == 0
+}
+
+// 向上取整返回最接近的 2 的整数次幂
 func cellToPowerOfTwo(x uint64) uint64 {
 	if x == 0 {
 		return 1
@@ -13,4 +22,23 @@ func cellToPowerOfTwo(x uint64) uint64 {
 	r |= r >> 16 // 32
 	r |= r >> 32 // 64
 	return r + 1
+}
+
+// 向上取整返回最接近的 2 的整数次幂
+func cellToPowerOfTwo2(x uint64) uint64 {
+	if x == 0 {
+		return 1
+	}
+
+	if isPowerOfTwo(x) {
+		return x
+	}
+
+	x |= x >> 1  // 2
+	x |= x >> 2  // 4
+	x |= x >> 4  // 8
+	x |= x >> 8  // 16
+	x |= x >> 16 // 32
+	x |= x >> 32 // 64
+	return x + 1
 }
