8e47cab130b0020953993cdab9f1c3ffd8baaf8a
laoshufeifei <laoshufeifei@qq.com>
2021-04-11 17:52:21 +0800

    update bits.go

--- src/other/bits/bits.go
+++ src/other/bits/bits.go
@@ -1,5 +1,9 @@
 package bits
 
+import (
+	"math"
+)
+
 func isPowerOfTwo(n uint64) bool {
 	if n == 0 {
 		return false
@@ -41,4 +45,95 @@ func cellToPowerOfTwo2(x uint64) uint64 {
 	x |= x >> 16 // 32
 	x |= x >> 32 // 64
 	return x + 1
+}
+
+// 向上取整返回最接近的 2 的整数次幂
+func cellToPowerOfTwo3(x uint64) uint64 {
+	if x == 0 {
+		return 1
+	}
+
+	if isPowerOfTwo(x) {
+		return x
+	}
+
+	if x > math.MaxInt64 {
+		return math.MaxUint64
+	}
+
+	for {
+		// x -= x & (-x)
+		x = x & (x - 1)
+		if isPowerOfTwo(x) {
+			return x << 1
+		}
+	}
+}
+
+// 一个二进制数字左边 0 的个数
+// https://www.jianshu.com/p/2c1be41f6e59
+func numberOfLeadingZeros(i uint64) uint64 {
+	if i == 0 {
+		return 64
+	}
+
+	var n uint64
+	n = 1
+
+	// 32
+	if i>>32 == 0 {
+		n += 32
+		i <<= 32
+	}
+
+	// 32 + 16
+	if i>>48 == 0 {
+		n += 16
+		i <<= 16
+	}
+
+	// 32 + 16 + 8
+	if i>>56 == 0 {
+		n += 8
+		i <<= 8
+	}
+
+	// 32 + 16 + 8 + 4
+	if i>>60 == 0 {
+		n += 4
+		i <<= 4
+	}
+
+	// 32 + 16 + 8 + 4 + 2
+	if i>>62 == 0 {
+		n += 2
+		i <<= 2
+	}
+
+	n -= i >> 63
+	return n
+}
+
+// 向上取整返回最接近的 2 的整数次幂
+// 构造出一个跟原来数字相当的 0000 1111，然后加 1 即可
+func cellToPowerOfTwo4(x uint64) uint64 {
+	if x == 0 {
+		return 1
+	}
+
+	if isPowerOfTwo(x) {
+		return x
+	}
+
+	if x > math.MaxInt64 {
+		return math.MaxUint64
+	}
+
+	// 如果是 2 的整数次幂那段没有提前返回，这里求左侧 0 的个数的时候需要使用 x - 1
+	// n := numberOfLeadingZeros(x-1)
+	n := numberOfLeadingZeros(x)
+
+	// 移位的时候为啥要 n - 1 呢？因为这里想用逻辑移位(左边补0)
+	// 0x7fffffffffffffff : 0111 1111 1111 1111........
+	return 0x7fffffffffffffff>>(n-1) + 1
 }
--- src/other/bits/bits_test.go
+++ src/other/bits/bits_test.go
@@ -15,6 +15,10 @@ func TestPowerTwo(t *testing.T) {
 	test.False(isPowerOfTwo(3))
 	test.True(isPowerOfTwo(4))
 	test.True(isPowerOfTwo(128))
+}
+
+func TestCellToPowerOfTwo(t *testing.T) {
+	test := assert.New(t)
 
 	test.Equal(cellToPowerOfTwo(0), uint64(1))
 	test.Equal(cellToPowerOfTwo(1), uint64(1))
@@ -28,6 +32,10 @@ func TestPowerTwo(t *testing.T) {
 	// 0x1fff: 0000 0001  1111 1111  ....
 	// 0x2000: 0000 0010  0000 0000  ....
 	test.Equal(cellToPowerOfTwo(0x1fff), uint64(0x2000))
+}
+
+func TestCellToPowerOfTwo2(t *testing.T) {
+	test := assert.New(t)
 
 	test.Equal(cellToPowerOfTwo2(0), uint64(1))
 	test.Equal(cellToPowerOfTwo2(1), uint64(1))
@@ -41,4 +49,55 @@ func TestPowerTwo(t *testing.T) {
 	// 0x1fff: 0000 0001  1111 1111  ....
 	// 0x2000: 0000 0010  0000 0000  ....
 	test.Equal(cellToPowerOfTwo2(0x1fff), uint64(0x2000))
+}
+
+func TestCellToPowerOfTwo3(t *testing.T) {
+	test := assert.New(t)
+
+	test.Equal(cellToPowerOfTwo3(0), uint64(1))
+	test.Equal(cellToPowerOfTwo3(1), uint64(1))
+	test.Equal(cellToPowerOfTwo3(2), uint64(2))
+	test.Equal(cellToPowerOfTwo3(3), uint64(4))
+	test.Equal(cellToPowerOfTwo3(4), uint64(4))
+	test.Equal(cellToPowerOfTwo3(5), uint64(8))
+	test.Equal(cellToPowerOfTwo3(100), uint64(128))
+	test.Equal(cellToPowerOfTwo3(129), uint64(256))
+
+	// 0x1fff: 0000 0001  1111 1111  ....
+	// 0x2000: 0000 0010  0000 0000  ....
+	test.Equal(cellToPowerOfTwo3(0x1fff), uint64(0x2000))
+}
+
+func TestCellToPowerOfTwo4(t *testing.T) {
+	test := assert.New(t)
+
+	test.Equal(cellToPowerOfTwo4(0), uint64(1))
+	test.Equal(cellToPowerOfTwo4(1), uint64(1))
+	test.Equal(cellToPowerOfTwo4(2), uint64(2))
+	test.Equal(cellToPowerOfTwo4(3), uint64(4))
+	test.Equal(cellToPowerOfTwo4(4), uint64(4))
+	test.Equal(cellToPowerOfTwo4(5), uint64(8))
+	test.Equal(cellToPowerOfTwo4(100), uint64(128))
+	test.Equal(cellToPowerOfTwo4(129), uint64(256))
+
+	// 0x1fff: 0000 0001  1111 1111  ....
+	// 0x2000: 0000 0010  0000 0000  ....
+	test.Equal(cellToPowerOfTwo4(0x1fff), uint64(0x2000))
+}
+
+func TestNumberOfLeadingZeros(t *testing.T) {
+	test := assert.New(t)
+
+	test.Equal(numberOfLeadingZeros(0), uint64(64))
+	test.Equal(numberOfLeadingZeros(1), uint64(63))
+	test.Equal(numberOfLeadingZeros(3), uint64(62))
+	test.Equal(numberOfLeadingZeros(0x8), uint64(60))
+	test.Equal(numberOfLeadingZeros(0xf), uint64(60))
+
+	test.Equal(numberOfLeadingZeros(0xffffff), uint64(40))
+	test.Equal(numberOfLeadingZeros(0xffffffff), uint64(32))
+	test.Equal(numberOfLeadingZeros(0xffffffffffff), uint64(16))
+	test.Equal(numberOfLeadingZeros(0xffffffffffffff), uint64(8))
+	test.Equal(numberOfLeadingZeros(0xfffffffffffffff), uint64(4))
+	test.Equal(numberOfLeadingZeros(0xffffffffffffffff), uint64(0))
 }
